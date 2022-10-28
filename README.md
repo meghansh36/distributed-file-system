7525719294d5f682b7bc1aab6b70a53f8a202c3c
meghansh <mgoel7@illinois.edu>
2022-10-28 16:29:34 -0500

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AwesomeSDFS
+# AwesomeFailureDetector
 
 
 
@@ -15,14 +15,14 @@ Already a pro? Just edit this README.md and make it your own. Want to make it ea
 
 ```
 cd existing_repo
-git remote add origin https://gitlab.engr.illinois.edu/mgoel7/awesomesdfs.git
+git remote add origin https://gitlab.engr.illinois.edu/mgoel7/awesomefailuredetector.git
 git branch -M main
 git push -uf origin main
 ```
 
 ## Integrate with your tools
 
-- [ ] [Set up project integrations](https://gitlab.engr.illinois.edu/mgoel7/awesomesdfs/-/settings/integrations)
+- [ ] [Set up project integrations](https://gitlab.engr.illinois.edu/mgoel7/awesomefailuredetector/-/settings/integrations)
 
 ## Collaborate with your team
 
---
+++ packets.py
@@ -0,0 +1,46 @@
+from enum import Enum
+import logging
+import pickle
+import struct
+import json
+from typing import Optional
+
+
+class PacketType(str, Enum):
+    """Current packet types supported by failure detector"""
+    PING = "01"
+    ACK = "10"
+    INTRODUCE = "00"
+
+
+class Packet:
+    """Custom packet type for failure detector"""
+
+    def __init__(self, sender: str, packetType: PacketType, data: dict):
+        self.data = data
+        self.type = packetType
+        self.sender = sender
+
+    def pack(self) -> bytes:
+        """Returns the bytes for packet"""
+        jsondata = json.dumps(self.data)
+        return struct.pack(f"i{255}s{2}si{2048}s", len(self.sender), self.sender.encode('utf-8'), self.type.encode('utf-8'), len(jsondata), jsondata.encode())
+
+        # pickled = pickle.dumps(self, pickle.HIGHEST_PROTOCOL)
+        # return pickled
+
+    @staticmethod
+    def unpack(recvPacket: bytes):
+        """Converts the bytes to Packet class"""
+        try:
+            unpacked_tuple: tuple[bytearray] = struct.unpack(
+                f"i{255}s{2}si{2048}s", recvPacket)
+            sender = unpacked_tuple[1][:unpacked_tuple[0]].decode('utf-8')
+            packetType = unpacked_tuple[2].decode('utf-8')
+            data = unpacked_tuple[4][:unpacked_tuple[3]].decode('utf-8')
+
+            # print(sender, packetType, data)
+            return Packet(sender, PacketType(packetType), json.loads(data))
+        except Exception as e:
+            logging.error(f"unknown bytes: {e}")
+            return None
